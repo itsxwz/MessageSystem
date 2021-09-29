a437020c4b14137498e3a5508048135753e9da26
86K <577635188@qq.com>
2021-09-29 10:09:29 +0800

    1.MessageSystem.Get()可以直接获取到消息管理类； 2.必须在OnDestroy()里面注销当前脚本所有注册的消息；

--- Assets/MessageSystem/Example/Notify.cs
+++ Assets/MessageSystem/Example/Notify.cs
@@ -1,45 +1,62 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityFramework;
 
 public class Notify : MonoBehaviour
 {
-    MessageManager msgManater;
+    bool canSkip;
 
-    private void Awake()
+    void Awake()
     {
-        msgManater = MessageManager.Get();
+        canSkip = SceneManager.GetActiveScene().name == "Test1";
     }
 
     private void OnGUI()
     {
+        /*
+            消息分发即告诉所有相关事件绑定方：你们要做的什么什么事情(key)，可以做啦
+        */
+
         if (GUILayout.Button("TurnRed"))
         {
             //无延迟单参数
-            msgManater.Notify<Color>("turnRed", Color.red);
+            MessageManager.Get().Notify<Color>("turnRed", Color.red);
         }
 
         if (GUILayout.Button("Rot"))
         {
             //无延迟双参数
-            msgManater.Notify<Quaternion, float>("rotate", Quaternion.identity, 3);
+            MessageManager.Get().Notify<Quaternion, float>("rotate", Quaternion.identity, 3);
         }
 
         if (GUILayout.Button("Destroy"))
         {
             //无参带key
-            msgManater.Notify("destroy");
+            MessageManager.Get().Notify("destroy");
         }
 
         if (GUILayout.Button("delyTurnBlue"))
         {
             //延迟单参数
-            msgManater.Notify<Color>("turnRed", this, Color.blue, 2);
+            //请注意，谁开启的延迟分发消息，谁负责停止协程，而不是响应方；
+            //注意发送和接收双方自身的存在周期，避免空引用
+            MessageManager.Get().Notify<Color>("turnRed", this, Color.blue, 2);
         }
 
         if (GUILayout.Button("打印7个参数的消息"))
         {
             //无延迟七参数(目前最大参数数量)
-            msgManater.Notify<int, int, int, int, int, int, int>("Test7Paras", 1, 2, 3, 4, 5, 6, 7);
+            MessageManager.Get().Notify<int, int, int, int, int, int, int>("Test7Paras", 1, 2, 3, 4, 5, 6, 7);
+        }
+
+        if (GUILayout.Button("跳转到场景1"))
+        {
+            SceneManager.LoadScene("Test1");
+        }
+
+        if (GUILayout.Button("跳转到场景2"))
+        {
+            SceneManager.LoadScene("Test2");
         }
     }
 }
---
+++ Assets/MessageSystem/Example/Register2.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityFramework;
+
+public class Register2 : MonoBehaviour
+{
+    MessageManager msgManager;
+
+    void Start()
+    {
+        msgManager = MessageManager.Get();
+
+        //需要响应什么事件，就去绑定；需要处理什么逻辑，就去书写
+        msgManager.AddListener<Color>("turnRed", OnReceive);
+        msgManager.AddListener<Quaternion, float>("rotate", OnReceive);
+        msgManager.AddListener("destroy", Destroy);
+        msgManager.AddListener<int,int,int,int,int,int,int>("Test7Paras", Print7Paras);
+    }
+
+    private void OnDestroy()
+    {
+        //脚本行为结束时，注销所有注册的事件
+        msgManager.RemoveListener<Color>("turnRed", OnReceive);
+        msgManager.RemoveListener<Quaternion, float>("rotate", OnReceive);
+        msgManager.RemoveListener("destroy", Destroy);
+        msgManager.RemoveListener<int,int,int,int,int,int,int>("Test7Paras", Print7Paras);
+        StopAllCoroutines();
+    }
+
+    void OnReceive(Color color)
+    {
+        GetComponent<Renderer>().material.color = color;
+    }
+
+    void OnReceive(Quaternion rot, float time)
+    {
+        Debug.Log(time);
+        transform.rotation = rot;
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    void Print7Paras(int i1, int i2, int i3, int i4, int i5, int i6, int i7)
+    {
+        Debug.Log($"{i1} {i2} {i3} {i4} {i5} {i6} {i7}");
+    }
+}
